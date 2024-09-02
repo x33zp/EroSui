75b67de9e7c328ee0f59a2aa93df4319d09a6af6
x33zp <zubbypeculiar@gmail.com>
2024-09-02 05:29:24 -0700

    Add: category model
    For different categories of products available, such as Men/Women

---
+++ models/category.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module creates a Category class"""
+from models.base_model import BaseModel
+
+
+class Category(BaseModel):
+    """Class for managing category objects"""
+
+    name = ""
+    description = ""
