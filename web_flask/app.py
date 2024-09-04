703a16c2e81df9e7be03fdc716307d966ba77215
x33zp <zubbypeculiar@gmail.com>
2024-09-04 04:53:24 +0100

    Updated: app
    Defined database obj

--- web_flask/app.py
+++ web_flask/app.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """Flask App"""
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from web_flask.views import app_views
 from web_flask.auth import app_auth
 
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'abcdefghijk lmnopqrst'
+app.config ['SQLALCHEMY_DATABASE_URL'] = f'sqlite:///{DB_NAME}'
+db.init_app(app)
+
 app.register_blueprint(app_views, url_prefix='/')
 app.register_blueprint(app_auth, url_prefix='/')
 
